bfb362d197ebe9d9a719748aedb4c9179e98fdb5
Lucasortega2 <lucas.ortega1999@gmail.com>
2025-03-21 18:36:54 -0300

    .

---
+++ src/pages/api/mercadopago.ts
@@ -0,0 +1,52 @@
+import type { APIRoute } from 'astro';
+import { MercadoPagoConfig, Preference } from 'mercadopago';
+
+export const prerender = false;
+
+export const GET: APIRoute = async () => {
+  return new Response(JSON.stringify({ success: true }), { status: 200 });
+};
+
+export const POST: APIRoute = async ({ request }) => {
+  console.log(import.meta.env.ACCESS_TOKEN_MP);
+  console.log(import.meta.env.SECRET_KEY_MP);
+
+  try {
+    const { items, payer } = await request.json();
+
+    const client = new MercadoPagoConfig({
+      accessToken: import.meta.env.ACCESS_TOKEN_MP,
+    });
+
+    const body = {
+      items,
+      back_urls: {
+        success: 'https://aeternum-tau.vercel.app/checkout/status',
+        failure: 'https://aeternum-tau.vercel.app/checkout/status',
+        pending: 'https://aeternum-tau.vercel.app/checkout/status',
+      },
+      payer: {
+        first_name: payer.firstName,
+        last_name: payer.lastName,
+        email: payer.email,
+      },
+      auto_return: 'approved',
+      notification_url: 'https://aeternum-tau.vercel.app/api/webhooks',
+    };
+    const preference = new Preference(client);
+
+    const result = await preference.create({ body });
+
+    return new Response(JSON.stringify({ id: result.id }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error('MercadoPago error:', error);
+    return new Response(
+      JSON.stringify({ success: false, message: error.message }),
+      {
+        status: 500,
+      },
+    );
+  }
+};
